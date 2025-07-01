903905cf9e53b8ea0616d575a31975097937036b
muraliSMD <muralis.1995@gmail.com>
2025-07-01 18:26:25 +0530

    ui updated

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,10 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const roboto = Roboto({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["400", "500", "700"], // you can include desired weights
+  variable: "--font-roboto",     // optional: use CSS variable
 });
 
 export const metadata = {
@@ -20,7 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${roboto.variable} ${roboto.variable} antialiased`}
       >
         {children}
       </body>
